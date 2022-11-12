788ae80fa13842779fe835b1c1121250b2e3e35a
taehoonyun <mn9069@naver.com>
2022-11-12 12:38:48 +0900

    edit responsive website

--- background.js
+++ background.js
@@ -10,56 +10,19 @@ function background() {
   bgImg.style.top = 0;
   bgImg.style.left = 0;
 }
-function bgAbout() {
+function bg() {
   const about = document.querySelector(".about");
   const bgImg = document.createElement("img");
-  bgImg.src = `https://i.postimg.cc/W4sCcXfb/About.jpg`;
+  bgImg.style.backgroundImage =
+    "linear-gradient(to bottom,#121d31,gray, #aaf0d1, white)";
+  bgImg.style.height = "420vh";
+  bgImg.style.width = "100%";
+  bgImg.style.zIndex = -2;
+  bgImg.style.position = "absolute";
+  bgImg.style.top = "99%";
+  bgImg.style.left = 0;
   about.appendChild(bgImg);
-  bgImg.style.height = "100%";
-  bgImg.style.width = "100%";
-  bgImg.style.zIndex = -1;
-  bgImg.style.position = "absolute";
-  bgImg.style.top = "100%";
-  // bgImg.style.left = 0;
 }
-function bgSkills() {
-  const skills = document.querySelector(".skills");
-  const bgImg = document.createElement("img");
-  bgImg.src = `https://i.postimg.cc/DZT0qW6M/skills.jpg`;
-  skills.appendChild(bgImg);
-  bgImg.style.height = "100%";
-  bgImg.style.width = "100%";
-  bgImg.style.zIndex = -1;
-  bgImg.style.position = "absolute";
-  bgImg.style.top = "200%";
-  // bgImg.style.left = 0;
-}
-function bgWork() {
-  const myWork = document.querySelector(".Work");
-  const bgImg = document.createElement("img");
-  bgImg.src = `https://i.postimg.cc/zGMvd2z6/1784147.jpg`;
-  myWork.appendChild(bgImg);
-  bgImg.style.height = "100%";
-  bgImg.style.width = "100%";
-  bgImg.style.zIndex = -1;
-  bgImg.style.position = "absolute";
-  bgImg.style.top = "300%";
-  // bgImg.style.left = 0;
-}
-function bgContact() {
-  const Contact = document.querySelector(".contact");
-  const bgImg = document.createElement("img");
-  bgImg.src = `https://i.postimg.cc/15Rzynv9/45863.jpg`;
-  Contact.appendChild(bgImg);
-  bgImg.style.height = "100%";
-  bgImg.style.width = "100%";
-  bgImg.style.zIndex = -1;
-  bgImg.style.position = "absolute";
-  bgImg.style.top = "400%";
-  // bgImg.style.left = 0;
-}
+
 background();
-bgAbout();
-bgSkills();
-bgWork();
-bgContact();
+bg();
--- main.js
+++ main.js
@@ -24,3 +24,43 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 1500) {
+    navBarMenu.style.color = "black";
+    navBarMenu.style.transition = "color 2s ease";
+    togglebtn.style.color = "black";
+    togglebtn.style.transition = "color 2s ease";
+  } else {
+    navBarMenu.style.color = "white";
+    navBarMenu.style.transition = "color 2s ease";
+    togglebtn.style.color = "white";
+    togglebtn.style.transition = "color 2s ease";
+  }
+});
+document.addEventListener("scroll", () => {
+  const HTML = document.querySelector(".HTML");
+  const CSS = document.querySelector(".CSS");
+  const JS = document.querySelector(".JS");
+  const React = document.querySelector(".React");
+  const Node = document.querySelector(".Node");
+
+  if (window.scrollY > 1400) {
+    HTML.style.width = "70%";
+    CSS.style.width = "60%";
+    JS.style.width = "70%";
+    React.style.width = "50%";
+    Node.style.width = "50%";
+    HTML.style.transitionTimingFunction = "easy-out";
+    HTML.style.transitionDuration = "3s";
+    CSS.style.transitionTimingFunction = "easy-out";
+    CSS.style.transitionDuration = "3s";
+    JS.style.transitionTimingFunction = "easy-out";
+    JS.style.transitionDuration = "3s";
+    React.style.transitionTimingFunction = "easy-out";
+    React.style.transitionDuration = "3s";
+    Node.style.transitionTimingFunction = "easy-out";
+    Node.style.transitionDuration = "3s";
+  } else {
+  }
+});
